69735951f078fdeeb7a8f97802543c63ab53bbfb
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-03-19 19:38:51 -0300

    PROJETO MELHORADO E FINALIZADO

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,58 +1,68 @@
 const emojis = [
-    '😀',
-    '😀',
-    '🦝',
-    '🦝',
-    '😇',
-    '😇',
-    '🤜',
-    '🤜',
-    '😛',
-    '😛',
-    '🐺',
-    '🐺',
-    '🐣',
-    '🐣',
-    '🎁',
-    '🎁',
-    '🍉',
-    '🍉',
-]
-let openCards = []
+  '😀',
+  '😀',
+  '🦝',
+  '🦝',
+  '😇',
+  '😇',
+  '🤜',
+  '🤜',
+  '😛',
+  '😛',
+  '🐺',
+  '🐺',
+  '🐣',
+  '🐣',
+  '🎁',
+  '🎁',
+  '🍉',
+  '🍉',
+];
 
-const shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
+const openCards = [];
 
-for (let index = 0; index < emojis.length; index++) {
-    const box = document.createElement('div')
-    box.className = 'item'
-    box.innerHTML = shuffleEmojis[index]
-    box.onclick = handleClick
-    document.querySelector('.game').appendChild(box)
+const shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+  const emojiBox = document.createElement('div');
+
+  emojiBox.className = 'item';
+  emojiBox.innerHTML = shuffleEmojis[i];
+  emojiBox.onclick = handleClick;
+
+  document.querySelector('#game').appendChild(emojiBox);
 }
 
-function handleClick() {
-    if (openCards.length < 2) {
-        this.classList.add('boxOpen')
-        openCards.push(this)
-    }
+function handleClick(event) {
+  if (event.target.classList.contains('openBox')) {
+    return;
+  }
 
-    if (openCards.length === 2) {
-        setTimeout(checkMatch, 500)
-    }
+  if (openCards.length < 2) {
+    event.target.classList.add('openBox');
+
+    openCards.push(event.target);
+  }
+
+  if (openCards.length === 2) {
+    setTimeout(checkMatch, 350);
+  }
 }
 
 function checkMatch() {
-    if (openCards[0].innerHTML === openCards[1].innerHTML) {
-        openCards[0].classList.add('boxMatch')
-        openCards[1].classList.add('boxMatch')
-    } else {
-        openCards[0].classList.remove('boxOpen')
-        openCards[1].classList.remove('boxOpen')
-    }
+  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add('boxMatch');
+    openCards[1].classList.add('boxMatch');
+  } else {
+    openCards[0].classList.remove('openBox');
+    openCards[1].classList.remove('openBox');
+  }
 
-    openCards = []
+  openCards.splice(0, 2);
 
-    if (document.querySelectorAll('.boxMatch').length == emojis.length) {
-        alert('Você venceu!')
-    }
+  if (document.querySelectorAll('.boxMatch').length === emojis.length) {
+    alert('Você venceu!');
+
+    document.location.reload(true);
+  }
 }
